9ee7f3cbbf5b05413e3f8b10fbd70e53a30a04ad
bugrice <manssik0820@gmail.com>
2023-08-17 10:15:54 +0900

    23.08.17useRef

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// useRef는 
+// 1. DOM에 직접 접근할 때 사용합니다.
+// 2. 지역 변수로 사용할 때 사용합니다.
+// useState는 값이 변경될 때 다시 랜더링을 한다.
+// useRef는 값이 변경되더라도 다시 랜더링을 하지 않습니다.
+
+import { useRef } from "react";
+import Input from "./components/input";
+import AutoCounter from "./components/AutoCounter";
+
+function App() {
+  const inputRef = useRef();
+
+  return(
+    <div>
+      <Input ref={inputRef}/>
+      <button onClick={() => inputRef.current.focus()}>focus</button>
+
+      <AutoCounter/>
+    </div>
+  );
+}
+
+export default App;
