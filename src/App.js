f8f6f8364ae52bde645141d07dc086cc17d47275
bugrice <manssik0820@gmail.com>
2023-08-17 11:48:23 +0900

    23.08.17style

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// 스타일을 적용하는 법
+// 1. 스타일시트를 이용하는 것
+// 2. Inlint style을 적용하는 것
+// 3. CSS in JS - emotion
+
+import Box from "./components/Box";
+
+function App() {
+  return <div>
+    <Box bgColor="red"/>
+  </div>
+}
+
+export default App;
